2f50f0236a2eb7b08a5fd6162d90c5ee94d01431
cts <zzfeicome@163.com>
2017-02-15 11:56:41 +0800

    新建项目

---
+++ testGithub/testGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGithub
+//
+//  Created by cts on 17/2/15.
+//  Copyright © 2017年 hhjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGithub/testGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGithub
+//
+//  Created by cts on 17/2/15.
+//  Copyright © 2017年 hhjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
